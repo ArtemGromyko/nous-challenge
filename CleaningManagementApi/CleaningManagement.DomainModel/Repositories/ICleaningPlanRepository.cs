96793a6a865abaef8ed1ad26c0cf6754eaf2f354
Artsiom Hramyka <artsiom.hramyka@innowise-group.com>
2022-03-23 09:05:49 +0300

    add domain model

---
+++ CleaningManagementApi/CleaningManagement.DomainModel/Repositories/ICleaningPlanRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleaningManagement.DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleaningManagement.DomainModel.Repositories
+{
+    public interface ICleaningPlanRepository
+    {
+        Task CreateCleaningPlanAsync(CleaningPlan cleaningPlan);
+        Task<List<CleaningPlan>> GetAllCleaningPlansForCustomerAsync(int customerId);
+        Task<CleaningPlan> GetCleaningPlanByIdAsync(Guid id);
+        Task DeleteCleaningPlanAsync(CleaningPlan cleaningPlan);
+    }
+}
