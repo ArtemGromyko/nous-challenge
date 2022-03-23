216ed78c1f3797ecf4b8edd4d26199f8db793877
Artsiom Hramyka <artsiom.hramyka@innowise-group.com>
2022-03-23 10:25:59 +0300

    add models

---
+++ CleaningManagementApi/CleaningManagement.Models/CleaningPlanCreationModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleaningManagement.Models
+{
+    internal class CleaningPlanCreationModel
+    {
+        [Required]
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [MaxLength(512)]
+        public string Description { get; set; }
+    }
+}
---
+++ CleaningManagementApi/CleaningManagement.Models/CleaningPlanResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CleaningManagement.Models
+{
+    public class CleaningPlanResponseModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int CustomerId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationdDate { get; set; }
+    }
+}
